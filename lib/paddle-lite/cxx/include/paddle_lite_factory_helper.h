de618fbb390517d9a022607882201c09edd9501e
WentsingNee <1205422966@qq.com>
2022-04-15 23:07:14 +0800

    solved link-time multiple definition problem

--- lib/paddle-lite/cxx/include/paddle_lite_factory_helper.h
+++ lib/paddle-lite/cxx/include/paddle_lite_factory_helper.h
@@ -30,11 +30,11 @@
 
 #define USE_LITE_OP(op_type__)       \
   extern int touch_op_##op_type__(); \
-  int LITE_OP_REGISTER_FAKE(op_type__) UNUSED = touch_op_##op_type__();
+  inline int LITE_OP_REGISTER_FAKE(op_type__) UNUSED = touch_op_##op_type__();
 
 #define USE_LITE_KERNEL(op_type__, target__, precision__, layout__, alias__) \
   extern int touch_##op_type__##target__##precision__##layout__##alias__();  \
-  int op_type__##target__##precision__##layout__##alias__##__use_lite_kernel \
+  inline int op_type__##target__##precision__##layout__##alias__##__use_lite_kernel \
       UNUSED = touch_##op_type__##target__##precision__##layout__##alias__();
 
 #define USE_MIR_PASS(name__)                      \
